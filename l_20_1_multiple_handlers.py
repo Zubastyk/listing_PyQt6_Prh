52af554feb5b8f2b8b127347d4958d4b5a29053c
Zubastyk <122399636+Zubastyk@users.noreply.github.com>
2023-02-26 20:40:58 +0300

    Add files via upload

---
+++ l_20_1_multiple_handlers.py
@@ -0,0 +1,30 @@
+from PyQt6 import QtWidgets
+import sys
+
+def on_clicked():
+    print("Кнопка нажата. Функция on_clicked()")
+    
+class MyClass():
+    def __init__(self, x=0):
+        self.x = x
+        
+    def __call__(self):
+        print("Кнопка нажата. Метод MyClass.__call__()")    
+        print("x = ", self.x)
+        
+    def on_clicked(self):
+        print("Кнопка нажата. Метод MyClass.on_clicked()")
+        
+obj = MyClass()
+app = QtWidgets.QApplication(sys.argv)
+button = QtWidgets.QPushButton("Нажми меня")
+# Назначаем обработчиком функцию
+button.clicked.connect(on_clicked)
+# Назначаем обработчиком метод класса
+button.clicked.connect(obj.on_clicked)
+# Назначаем обработчиком ссылку на объект класса
+button.clicked.connect(MyClass(10))
+# Назначаем обработчиком анонимную функцию
+button.clicked.connect(lambda: MyClass(5)())
+button.show()
+sys.exit(app.exec())
